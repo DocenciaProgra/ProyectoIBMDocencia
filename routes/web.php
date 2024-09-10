25640a272ce22ee9460910f46bc5e15817102f1f
DocenciaProgra <institutoceicom@gmail.com>
2024-09-10 09:11:21 -0400

    Inicio de Proyecto
    Creamos la estructura de laravel nombre de proyecto en blanco

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/compra', function () {
+    return view('welcome');
+});
+Route::get('/compra', function () {
+    return view('compra');
+});
